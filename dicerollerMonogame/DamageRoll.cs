1d04f1c0bbe58e9be2688719c825f6e4d94d3947
Billboard-SP <A00312828@mycambrian.ca>
2025-06-01 19:08:51 -0400

    variables made

---
+++ dicerollerMonogame/DamageRoll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dicerollerMonogame
+{
+    internal class DamageRoll
+    {
+        private int diceCount, diceSides, modifier;
+        private string damageType;
+        private List<(int count, int sides, string type)> miscSources;
+        private Dictionary<string, int> damageByType;
+        private int lastTotalDamage;
+
+
+    }
+}
